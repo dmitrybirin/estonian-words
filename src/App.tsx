5106189b93b7217afbe59b21760f1fc6ed7fa200
Dmitry Birin <dmitry.birin@pipedrive.com>
2021-02-27 17:28:15 +0200

    a bit more styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,28 @@ import React, { useState } from 'react'
 import './App.css'
 import { words } from './data/words'
 
+
+const Header = () => {
+  return <div className="header"><h1>Estonian words</h1></div>
+}
+
 function App() {
 
   const [inputText, setInputText] =  useState('')
 
   return (
-    <div className="list">
+    <>
+    <Header/>
+    <div className="search">
       <p>{words.length}</p>
-        <input type="text" value={inputText} onChange={(event) => setInputText(event.target.value)} />
+          <input type="text" value={inputText} onChange={(event) => setInputText(event.target.value)} />
+        </div>
+    <div className="list">
+      
         {words.filter(word => word.ruTranslation.includes(inputText) || word.forms.first.includes(inputText)).map(word => <div key={word.forms.first} className="card" style={{color:'red'}}>{word.ruTranslation}</div>)
         }
     </div>
+    </>
   )
 }
 
