87264a8069dc45f81804223507ebf4120f17e071
Dmitry Birin <dmitry.birin@pipedrive.com>
2021-02-27 20:56:18 +0200

    training with checking

--- src/Training.tsx
+++ src/Training.tsx
@@ -5,27 +5,72 @@ import { words } from './data/words';
 import { Word } from './types';
 import { getRandomElement, pseudoShuffle } from './utils';
 
+enum TestResult {
+	ONGOING,
+	SUCCESS,
+	FAILURE
+}
+
 const TrainingContainer = styled(CenteredContainer)`
 	flex-direction: column;
 `;
 
 const Variant = styled.p`
 	padding: 10px;
+	cursor: pointer;
 `;
 
-const getVariants = (answer: Word, words: Word[]) => {
-	return pseudoShuffle([answer, getRandomElement<Word>(words), getRandomElement<Word>(words)]);
+const Result: React.FC<{ result: TestResult }> = ({ result }) => {
+	switch (result) {
+		case TestResult.ONGOING:
+			return <p>🤔</p>;
+
+		case TestResult.SUCCESS:
+			return <p>🎉</p>;
+
+		case TestResult.FAILURE:
+			return <p>😭</p>;
+
+		default:
+			return null;
+	}
+};
+
+const getVariants = (answer: Word, words: Word[], numberOfVariants = 3) => {
+	const variants = [answer];
+
+	// TODO could use some recursive love
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	for (const _ of Array(numberOfVariants - 1)) {
+		const poolOfWords = words.filter((word) => !variants.map((variant) => variant.id).includes(word.id));
+
+		variants.push(getRandomElement<Word>(poolOfWords));
+	}
+
+	return pseudoShuffle(variants);
+};
+
+const getTestResult = (answer: Word, currentWord: Word) => {
+	if (answer.id === currentWord.id) {
+		return TestResult.SUCCESS;
+	} else {
+		return TestResult.FAILURE;
+	}
 };
 
 export function Training() {
 	const [counter, setCounter] = React.useState(0);
+	const [result, setResult] = React.useState(TestResult.ONGOING);
 	React.useEffect(() => {
 		const interval = setInterval(() => setCounter((counter) => counter + 1), 1000);
 		return () => clearInterval(interval);
 	}, []);
 	const [word, setWord] = React.useState(getRandomElement<Word>(words));
 
-	React.useEffect(() => setVariants(getVariants(word, words)), [word]);
+	React.useEffect(() => {
+		setVariants(getVariants(word, words));
+		setResult(TestResult.ONGOING);
+	}, [word]);
 
 	const [variants, setVariants] = React.useState(
 		pseudoShuffle([word, getRandomElement<Word>(words), getRandomElement<Word>(words)])
@@ -34,12 +79,15 @@ export function Training() {
 	return (
 		<TrainingContainer>
 			<p>{counter}</p>
+			<Result result={result} />
 			<p>{word.ruTranslation}</p>
 			<p>?</p>
 
 			<CenteredContainer>
 				{variants.map((variant) => (
-					<Variant key={variant.id}>{variant.forms.first}</Variant>
+					<Variant key={variant.id} onClick={() => setResult(getTestResult(variant, word))}>
+						{variant.forms.first}
+					</Variant>
 				))}
 			</CenteredContainer>
 
