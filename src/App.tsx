dbfd977f620d2187e357c60835e64aa8390f4994
Dmitry Birin <dmitry.birin@pipedrive.com>
2021-02-27 19:37:06 +0200

    router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Card } from './Card';
-import { CenteredContainer } from './common-styles';
-import { words } from './data/words';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const List = styled.div`
-	display: grid;
-	grid-gap: 16px;
-	justify-content: center;
-	padding: 16px;
-	grid-template-columns: repeat(auto-fill, 300px);
-`;
+import { Dictionary } from './Dictionary';
 
-const Header = () => {
+export function App() {
 	return (
-		<CenteredContainer>
-			<h1>Estonian words</h1>
-		</CenteredContainer>
-	);
-};
-
-function App() {
-	const [inputText, setInputText] = React.useState('');
-
-	return (
-		<>
-			<Header />
-			<CenteredContainer>
-				<p>Number in the dict: {words.length}</p>
-			</CenteredContainer>
-			<CenteredContainer>
-				<input type="text" value={inputText} onChange={(event) => setInputText(event.target.value)} />
-			</CenteredContainer>
-			<List>
-				{words
-					.filter((word) => word.ruTranslation.includes(inputText) || word.forms.first.includes(inputText))
-					.map((word) => (
-						<Card key={word.forms.first} word={word} />
-					))}
-			</List>
-		</>
+		<Router>
+			<Route path="/" exact component={Dictionary} />
+			<Route path="/dictionary/:searchId" exact component={Dictionary} />
+			<Route path="/dictionary" exact component={Dictionary} />
+		</Router>
 	);
 }
-
-export default App;
---
+++ src/Dictionary.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import { Card } from './Card';
+import { CenteredContainer } from './common-styles';
+import { words } from './data/words';
+
+const List = styled.div`
+	display: grid;
+	grid-gap: 16px;
+	justify-content: center;
+	padding: 16px;
+	grid-template-columns: repeat(auto-fill, 300px);
+`;
+
+const Header = () => {
+	return (
+		<CenteredContainer>
+			<h1>Estonian words</h1>
+		</CenteredContainer>
+	);
+};
+
+export function Dictionary() {
+	const { searchId } = useParams<{ searchId: string }>();
+	const [inputText, setInputText] = React.useState(searchId ?? '');
+
+	return (
+		<>
+			<Header />
+			<CenteredContainer>
+				<p>Number in the dict: {words.length}</p>
+			</CenteredContainer>
+			<CenteredContainer>
+				<input type="text" value={inputText} onChange={(event) => setInputText(event.target.value)} />
+			</CenteredContainer>
+			<List>
+				{words
+					.filter((word) => word.ruTranslation.includes(inputText) || word.forms.first.includes(inputText))
+					.map((word) => (
+						<Card key={word.forms.first} word={word} />
+					))}
+			</List>
+		</>
+	);
+}
