220fce30d1dcb29a3682ab2e2ec8547ee8adce37
Dmitry Birin <dmitry.birin@pipedrive.com>
2021-03-09 21:54:20 +0200

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,12 @@ import { Dictionary } from './Dictionary';
 import { Training } from './Training';
 import { VariantsGame } from './Training/VariantsGame';
 
+const MainContainer = styled.div`
+	display: grid;
+	height: 100vh;
+	grid-template-rows: auto 1fr auto;
+`;
+
 const Header = () => {
 	return (
 		<CenteredContainer>
@@ -15,23 +21,50 @@ const Header = () => {
 	);
 };
 
+const FooterContainer = styled(CenteredContainer)`
+	padding: 10px;
+	justify-content: flex-end;
+`;
+
+const Footer = () => {
+	return (
+		<FooterContainer>
+			Made with ❤️ All data from{' '}
+			<a href="https://sonaveeb.ee/" target="_blank">
+				Sõnaveeb
+			</a>{' '}
+			and
+			<a href="https://www.eki.ee/EN/">Institute of the Estonian Language</a>
+		</FooterContainer>
+	);
+};
+
+const Content = styled.div`
+	flex: 1;
+`;
+
 const Links = styled(CenteredContainer)`
 	justify-content: space-around;
 `;
 
 export function App() {
 	return (
-		<Router>
-			<Header />
-			<Links>
-				<Link to="/dictionary">Dictionary</Link>
-				<Link to="/training">Training</Link>
-			</Links>
-			<Route path="/" exact component={Dictionary} />
-			<Route path="/dictionary/:searchId" exact component={Dictionary} />
-			<Route path="/dictionary" exact component={Dictionary} />
-			<Route path="/training" exact component={Training} />
-			<Route path="/training/variants/:level" exact component={VariantsGame} />
-		</Router>
+		<MainContainer>
+			<Router>
+				<Header />
+				<Content>
+					<Links>
+						<Link to="/dictionary">Dictionary</Link>
+						<Link to="/training">Training</Link>
+					</Links>
+					<Route path="/" exact component={Dictionary} />
+					<Route path="/dictionary/:searchId" exact component={Dictionary} />
+					<Route path="/dictionary" exact component={Dictionary} />
+					<Route path="/training" exact component={Training} />
+					<Route path="/training/variants/:level" exact component={VariantsGame} />
+				</Content>
+				<Footer />
+			</Router>
+		</MainContainer>
 	);
 }
